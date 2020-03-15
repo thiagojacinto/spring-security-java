76c550a35153c8420784755013630fb7a75dfe0b
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-03-15 14:50:44 -0300

    [init] initial commit

---
+++ backend/src/test/java/br/securityjava/securitylearn/SecurityLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package br.securityjava.securitylearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
