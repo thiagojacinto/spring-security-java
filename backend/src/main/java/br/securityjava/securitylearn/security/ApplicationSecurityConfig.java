4eaeab7420d99db28f22e5c0e075da1f79bd3884
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-03-15 16:26:42 -0300

    [new] whitlist some access before auth + using password encoder

--- backend/src/main/java/br/securityjava/securitylearn/security/ApplicationSecurityConfig.java
+++ backend/src/main/java/br/securityjava/securitylearn/security/ApplicationSecurityConfig.java
@@ -1,14 +1,31 @@
 package br.securityjava.securitylearn.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	// Attributes
+	private final PasswordEncoder passwordEncoder;	// calls encoder to deal with password
+	
+	// Constructor
+	@Autowired
+	public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+
+		this.passwordEncoder = passwordEncoder;
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
@@ -18,11 +35,28 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 		
 		http
 			.authorizeRequests()
+			.antMatchers("/", "index", "/css/index.css", "/js/index.js")	// Used to whitelist access permission
+			.permitAll()	// then, permits access to predefined Matchers
 			.anyRequest()
 			.authenticated()
 			.and()
 			.httpBasic();
 		
+	}
+	
+	@Override
+	@Bean
+	protected UserDetailsService userDetailsService() {
+		
+		UserDetails thisUserBuilder = User.builder()
+			.username("thiagojacinto")
+			.password(passwordEncoder.encode("password"))
+			.roles("USER") 	// ROLE_USER
+			.build();
+		
+		return new InMemoryUserDetailsManager(
+				thisUserBuilder
+				);
 	}
 
 }
