32643ed1080d3055233979269e4d5ab939bcffb2
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-03-16 21:00:08 -0300

    [feat] API methods placeholders, but configurated for SpringBoot

--- backend/src/main/java/br/securityjava/securitylearn/user/UserManagementController.java
+++ backend/src/main/java/br/securityjava/securitylearn/user/UserManagementController.java
@@ -3,6 +3,12 @@ package br.securityjava.securitylearn.user;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,12 +24,32 @@ public class UserManagementController {
 	
 	// Methods
 	
+	@GetMapping
 	public List<User> getAllUsers() {
+		
+		// TODO real implementation
+		
+		System.out.println("getAllUsers()");
+		
 		return USERS;
 	}
 	
-	public void registerUser(User user) {
-		
+	@PostMapping
+	public void registerUser(@RequestBody User user) {
+		System.out.println(user);	// Placeholder
+		System.out.println("registerUser()");
+	}
+	
+	@DeleteMapping(path = "{userId}")
+	public void deleteUser(@PathVariable("userId") Integer userId) {
+		System.out.println("Delete: ID = " + userId);	// Placeholder
+		System.out.println("deleteUser()");
+	}
+	
+	@PutMapping(path = "{userId}")
+	public void updateUser(@PathVariable("userId") Integer userId, @RequestBody User updatedUser ) {
+		System.out.println(String.format("ID = %s; Updated = %s", userId, updatedUser));	// Placeholder
+		System.out.println("updateUser()");
 	}
 
 }
