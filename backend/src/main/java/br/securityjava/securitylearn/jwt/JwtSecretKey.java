9eb018f5d3de17c06b8255fb950692e21449a5ac
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-04-08 16:43:37 -0300

    [fix] use Autowired annot. into constructor

--- backend/src/main/java/br/securityjava/securitylearn/jwt/JwtSecretKey.java
+++ backend/src/main/java/br/securityjava/securitylearn/jwt/JwtSecretKey.java
@@ -2,6 +2,7 @@ package br.securityjava.securitylearn.jwt;
 
 import javax.crypto.SecretKey;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +13,7 @@ public class JwtSecretKey {
 	
 	private final JwtConfig jwtConfig;
 	
+	@Autowired
 	public JwtSecretKey(JwtConfig jwtConfig) {
 		this.jwtConfig = jwtConfig;
 	}
